e7b929db2a663718ef0781ac9ce56a3720775d98
Ciaran Archer <ciaran.archer@gmail.com>
2012-05-30 20:26:17 +0100

    Converted to coffee script

--- specs/roman_spec.js
+++
@@ -1,9 +0,0 @@
-var test = require('../lib/roman');
-//var jasmine = require('jasmine-node');
-
-describe('test run', function() {            
-  it('obj should be created', function() {
-    x = new TestClass()
-    expect(x.myValue()).toEqual(100);
-  });
-});
