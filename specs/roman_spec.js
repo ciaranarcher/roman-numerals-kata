73b2b83dc6c481325831a184576c1ce2f1a15248
Ciaran Archer <ciaran.archer@gmail.com>
2012-05-30 20:04:57 +0100

    Initial commit of JS files.

---
+++ specs/roman_spec.js
@@ -0,0 +1,9 @@
+var test = require('../lib/roman');
+var jasmine = require('jasmine-node');
+
+describe('test run', function() {            
+  it('obj should be created', function() {
+    x = new TestClass()
+    expect(x.myValue()).toEqual(100);
+  });
+});
